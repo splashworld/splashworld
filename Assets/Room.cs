5bf55fe2a400a431fae3fbfc2a54f612f2c59ec5
Kimchee4Me <esther.m.malone@gmail.com>
2016-11-05 23:02:50 -0400

    Trees

--- Assets/Room.cs
+++ Assets/Room.cs
@@ -2,29 +2,23 @@
 using System.Collections;
 
 public class Room : MonoBehaviour {
-	int min = -20;
-	int max =20;
+	int min = -50;
+	int max =50;
 	float timeLeft=30f;
 	GUIStyle style;
 	public int score;
 
 
 	public GameObject target1;
+	public GameObject tree;
 
 	void Start () {
 		style = new GUIStyle();
 		style.fontSize=20;
 		score = 0;
 
-
-		for (int i = 0; i < 30; i++) {
-			float x = Random.Range (min, max);
-			float z = Random.Range (min, max);
-			float y = Random.Range (1f, max/2);
-			Instantiate(target1);
-			target1.transform.position = new Vector3 (x, y, z);
-			target1.gameObject.GetComponent<TargetBehavior> ().r = this;
-		}
+		initTargets ();
+		initTrees ();
 	}
 	
 	// Update is called once per frame
@@ -41,5 +35,30 @@ public class Room : MonoBehaviour {
 		GUI.Label (new Rect (5, 5, 200, 200), timeLeft.ToString ("F0"), style);
 		GUI.Label (new Rect (Screen.width - 200, 5, 200, 200), score.ToString(), style); 
 		GUI.Label(new Rect(Screen.width/2-5, Screen.height/2-5,10,10), "+",style);
+	}
+
+	void initTargets()
+	{
+		for (int i = 0; i < 30; i++) {
+			float x = Random.Range (min, max);
+			float z = Random.Range (min, max);
+			float y = Random.Range (1f, max/2);
+			Instantiate(target1);
+			target1.transform.position = new Vector3 (x, y, z);
+			target1.gameObject.GetComponent<TargetBehavior> ().r = this;
+		}
+	}
+
+	void initTrees()
+	{
+		for (int i = 1; i < 10; i++) {
+			float x = Random.Range (min, max);
+			float z = Random.Range (min, max);
+
+			Instantiate(tree);
+			tree.transform.position = new Vector3 (x, 0, z);
+			tree.gameObject.GetComponent<Tree> ().r = this;
+			tree.gameObject.GetComponent<Tree> ().init ();
+		}
 	}
 }
